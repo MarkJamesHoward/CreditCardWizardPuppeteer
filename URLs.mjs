889821b31148af0112d97d902d9769d52d1bf969
MarkJamesHoward <markjameshoward@hotmail.co.uk>
2020-07-05 12:46:41 +1200

    Completed Westpac cards

--- URLs.mjs
+++ URLs.mjs
@@ -17,3 +17,34 @@ export const ANZCashbackVisaID = "1YxRMcFZosLLv26QkWP9m5";
 export const ANZCashbackVisaPlatinumURL =
   "https://www.anz.co.nz/personal/credit-cards/cashback-platinum/";
 export const ANZCashbackVisaPlatinumID = "7wsckFEHPxXPkMaJ8x2vny";
+
+//------------------------------------------------------------------
+//WESTPAC
+
+export const WestpacLowRateMastercardURL =
+  "https://www.westpac.co.nz/credit-cards/low-rate-mastercard/";
+export const WestpacLowRateMastercardID = "7qlzBcLkpP8FGuqNnEis04";
+
+export const WestpacHotpointsWorldMastercardURL =
+  "https://www.westpac.co.nz/credit-cards/hotpoints/hotpoints-world-mastercard/";
+export const WestpacHotpointsWorldMastercardID = "5pSDuHcsg9wsq3LsQ7Y69E";
+
+export const WestpacHotpointsPlatinumMastercardURL =
+  "https://www.westpac.co.nz/credit-cards/hotpoints/hotpoints-platinum-mastercard/";
+export const WestpacHotpointsPlatinumMastercardID = "6KtBACOrnmhr5UATIm6y4n";
+
+export const WestpacHotpointsMastercardURL =
+  "https://www.westpac.co.nz/credit-cards/hotpoints/hotpoints-mastercard/";
+export const WestpacHotpointsMastercardID = "5o7F9tuHcsObebOGDsCQ9f";
+
+export const WestpacAirpointsMastercardURL =
+  "https://www.westpac.co.nz/credit-cards/airpoints/airpoints-mastercard/";
+export const WestpacAirpointsMastercardID = "6QytVSylDciSRatyhcJ9gU";
+
+export const WestpacAirpointsWorldMastercardURL =
+  "https://www.westpac.co.nz/credit-cards/airpoints/airpoints-world-mastercard/";
+export const WestpacAirpointsWorldMastercardID = "2ZGbp1kxwuVP2yLcnpfccZ";
+
+export const WestpacAirpointsPlatinumMastercardURL =
+  "https://www.westpac.co.nz/credit-cards/airpoints/airpoints-platinum-mastercard/";
+export const WestpacAirpointsPlatinumMastercardID = "4xLghP5t0WI7Z3IEo3Kdgd";
--- main.mjs
+++ main.mjs
@@ -1,7 +1,5 @@
 import {
-  UpdateWestpacAirpointsPlatinumMastercard,
-  UpdateWestpacAirpointsMastercard,
-  UpdateWestpacAirpointsWorldMastercard,
+  UpdateWestpac,
   FindWestPacCashRate,
   FindWestPacPurchaseRate,
 } from "./westpac.mjs";
@@ -13,6 +11,8 @@ import {
   UpdateANZCashbackVisaPlatinum,
   UpdateANZAirpointsVisa,
 } from "./ANZ.mjs";
+
+import * as URL from "./URLs.mjs";
 
 import puppeteer from "puppeteer";
 import contentful from "contentful-management";
@@ -46,13 +46,57 @@ async function Start() {
   await CreateContentfulClient();
   console.log("Logged in successfully");
 
+  console.log("--WESTPAC-------------------------------");
+  await UpdateWestpac(
+    page,
+    env,
+    URL.WestpacLowRateMastercardURL,
+    URL.WestpacLowRateMastercardID
+  );
   console.log("----------------------------------------");
-  await UpdateWestpacAirpointsPlatinumMastercard(page, env);
+
+  await UpdateWestpac(
+    page,
+    env,
+    URL.WestpacHotpointsWorldMastercardURL,
+    URL.WestpacHotpointsWorldMastercardID
+  );
   console.log("----------------------------------------");
-  await UpdateWestpacAirpointsMastercard(page, env);
+  await UpdateWestpac(
+    page,
+    env,
+    URL.WestpacHotpointsPlatinumMastercardURL,
+    URL.WestpacHotpointsPlatinumMastercardID
+  );
   console.log("----------------------------------------");
-  await UpdateWestpacAirpointsWorldMastercard(page, env);
+  await UpdateWestpac(
+    page,
+    env,
+    URL.WestpacHotpointsMastercardURL,
+    URL.WestpacHotpointsMastercardID
+  );
   console.log("----------------------------------------");
+  await UpdateWestpac(
+    page,
+    env,
+    URL.WestpacAirpointsPlatinumMastercardURL,
+    URL.WestpacAirpointsPlatinumMastercardID
+  );
+  console.log("----------------------------------------");
+  await UpdateWestpac(
+    page,
+    env,
+    URL.WestpacAirpointsMastercardURL,
+    URL.WestpacAirpointsMastercardID
+  );
+  console.log("----------------------------------------");
+  await UpdateWestpac(
+    page,
+    env,
+    URL.WestpacAirpointsWorldMastercardURL,
+    URL.WestpacAirpointsWorldMastercardID
+  );
+  console.log("--ANZ-----------------------------------");
   await UpdateANZCashbackVisaPlatinum(page, env);
   console.log("----------------------------------------");
   await UpdateANZCashbackVisa(page, env);
